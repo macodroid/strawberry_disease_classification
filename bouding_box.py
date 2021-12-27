40e22c6a4f073c8df5a5acec185470864d689227
Andrej Macko <andrej.macko@siemens-healthineers.com>
2021-12-27 13:52:15 +0100

    displaying all bounding boxes in image

--- bouding_box.py
+++ bouding_box.py
@@ -3,22 +3,34 @@ import cv2 as cv
 import json
 
 
+# get just one bounding box
 def get_points(json_file_path):
     with open(json_file_path) as handle:
         dictdump = json.loads(handle.read())
     return np.asarray(dictdump['shapes'][0]['points'], np.int32)
 
 
-if __name__ == '__main__':
-    path_image = 'data/train/powdery_mildew_leaf517.jpg'
-    path_image_json = 'data/train/powdery_mildew_leaf517.json'
+# get all bounding boxes
+def get_all_points(json_file_path):
+    points = []
+    with open(json_file_path) as handle:
+        dictdump = json.loads(handle.read())
+    boxes = dictdump['shapes']
+    for box_points in boxes:
+        points.append(box_points['points'])
+    return points
 
-    bounding_box_points = get_points(path_image_json)
+
+if __name__ == '__main__':
+    path_image = 'data/train/angular_leafspot1.jpg'
+    path_image_json = 'data/train/angular_leafspot1.json'
+
+    bounding_box_points = get_all_points(path_image_json)
 
     img = cv.imread(path_image)
-    cv.polylines(img, [bounding_box_points], isClosed=True, color=(255, 0, 0), thickness=2)
+    for points in bounding_box_points:
+        cv.polylines(img, [np.asarray(points, np.int32)], isClosed=True, color=(255, 0, 0), thickness=2)
 
     cv.imshow("Strawberry", img)
     cv.waitKey(0)
     cv.destroyAllWindows()
-

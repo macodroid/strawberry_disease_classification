cc6d45f2099a0adddc0b3ba246fae893df8bae5a
Andrej Macko <andrej.macko@siemens-healthineers.com>
2021-12-30 02:05:01 +0100

    training and validation loss

--- convolution_neural_network.py
+++ convolution_neural_network.py
@@ -10,8 +10,11 @@ from torch.utils.data import DataLoader
 # Setting the device for using the CUDA if is possible
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# keeping-track-of-losses
+train_losses = []
+valid_losses = []
+
 # Setting hyperparameters
-input_size = None
 number_of_classes = 7
 learning_rate = 0.001
 batch_size = 32
@@ -26,13 +29,14 @@ transform = transforms.Compose(
 
 # Loading dateset (loading training, testing and validating sets)
 train_set = StrawberryDataset(csv_file='dataset/data_train.csv', image_dir='dataset/train',
-                              transform=transforms.ToTensor())
+                              transform=transform)
 test_set = StrawberryDataset(csv_file='dataset/data_test.csv', image_dir='dataset/test',
                              transform=transform)
-val_set = StrawberryDataset(csv_file='dataset/data_val.csv', image_dir='dataset/val',
-                            transform=transform)
+valid_set = StrawberryDataset(csv_file='dataset/data_val.csv', image_dir='dataset/val',
+                              transform=transform)
 
 train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
+valid_loader = DataLoader(dataset=valid_set, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
 
 model = ConvNet().to(device)
@@ -42,7 +46,9 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 n_total_steps = len(train_loader)
 for epoch in range(num_epoch):
-    for i, (images, labels) in enumerate(train_loader):
+    train_loss = 0.0
+    valid_loss = 0.0
+    for images, labels in train_loader:
         images = images.to(device)
         labels = labels.to(device)
 
@@ -53,4 +59,23 @@ for epoch in range(num_epoch):
         loss.backward()
         optimizer.step()
 
-        print(f'Epoch [{epoch + 1}/{num_epoch}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+        train_loss += loss.item() * images.size(0)
+        #print(f'Epoch [{epoch + 1}/{num_epoch}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+
+    model.eval()
+    for image, label in valid_loader:
+        image = image.to(device)
+        label = label.to(device)
+        output = model(image)
+        loss = criterion(output, label)
+        # update-average-validation-loss
+        valid_loss += loss.item() * image.size(0)
+    # calculate-average-losses
+    train_loss = train_loss / len(train_loader.sampler)
+    valid_loss = valid_loss / len(valid_loader.sampler)
+    train_losses.append(train_loss)
+    valid_losses.append(valid_loss)
+
+    # print-training/validation-statistics
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+        epoch, train_loss, valid_loss))

d2a168ff23c2bd366028066d03d96e390a1b874b
maco <macko.andrej1@gmail.com>
2022-01-02 12:41:34 +0100

    added new layers to model accuracy cca. 61%

--- model.py
+++ model.py
@@ -1,27 +1,42 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional
-import torchvision
-import torchvision.transforms as transforms
+import torch.nn.functional as F
 
 
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        # convolution layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(3, 3), stride=(2, 2), padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), stride=(2, 2), padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(2, 2), padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(2, 2), padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=(2, 2), padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        # pooling
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.dropout = nn.Dropout2d(0.5)
+
         # Fully connected layers
-        self.fc1 = nn.Linear(in_features=16 * 101 * 101, out_features=120)
-        self.fc2 = nn.Linear(in_features=120, out_features=84)
-        self.fc3 = nn.Linear(in_features=84, out_features=7)
+        self.fc1 = nn.Linear(in_features=64 * 6 * 6, out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024, out_features=512)
+        self.fc3 = nn.Linear(in_features=512, out_features=7)
 
     def forward(self, x):
-        x = self.pool(nn.functional.relu(self.conv1(x)))
-        x = self.pool(nn.functional.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 101 * 101)
+        # Here we are connecting them
+        x = self.pool(nn.functional.relu(self.bn1(self.conv1(x))))
+        x = self.pool(nn.functional.relu(self.bn2(self.conv2(x))))
+        x = self.pool(nn.functional.relu(self.bn3(self.conv3(x))))
+        # x = self.pool(nn.functional.relu(self.bn4(self.conv4(x))))
+        # x = self.pool(nn.functional.relu(self.conv4(x)))
+
+        x = x.view(-1, 64 * 6 * 6)
         x = nn.functional.relu(self.fc1(x))
-        x = nn.functional.relu(self.fc2(x))
+        x = nn.functional.relu(self.fc2(self.dropout(x)))
         x = self.fc3(x)
 
         return x
